050bdd7173f0e040522aad4f6fc18140f9d1f5dd
Pedro Gavioli <pedrohgavioli@gmail.com>
2026-06-17 15:14:15 -0300

    Add odds fetching functionality and configuration settings

---
+++ odds.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+url = 'https://api.the-odds-api.com/v4/sports/soccer_fifa_world_cup/odds/?apiKey=805fcc42035ee90840f0bdc5cb741c2b&regions={}&markets=h2h,spreads&oddsFormat=decimal'
+regions = ['br', 'us', 'eu', 'au']
+
+all_odds = []
+
+for region in regions:
+  url_with_region = url.format(region)
+
+  response = requests.get(url_with_region)
+  if response.status_code == 200:
+      data = response.json()
+      
+      for match in data:
+          match_info = {
+              'home_team': match['home_team'],
+              'away_team': match['away_team'],
+              'commence_time': match['commence_time']
+          }
+          
+          for bookmaker in match['bookmakers']:
+              for market in bookmaker['markets']:
+                  if market['key'] == 'h2h':
+                      for outcome in market['outcomes']:
+                          if outcome['name'] == match['home_team']:
+                              match_info['home_odds'] = outcome['price']
+                          elif outcome['name'] == match['away_team']:
+                              match_info['away_odds'] = outcome['price']
+          
+          all_odds.append(match_info)
