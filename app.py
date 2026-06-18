5d264c4acfa2a4a8454ba20fd47306d3198c1c92
Pedro Gavioli <pedrohgavioli@gmail.com>
2026-06-18 11:38:16 -0300

    Fix team selection logic in main function and add configuration file for theme settings

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
 
     st.subheader("Simular Confronto Específico")
     # Extrai os nomes únicos de todos os times para os selectboxes
-    teams = pd.concat([historical_data['team1'], historical_data['team2']]).unique()
+    teams = list(pd.concat([historical_data['team1'], historical_data['team2']]).unique())
     teams.sort()
     
     banca = st.number_input("Sua Banca Atual (R$)", min_value=1.0, value=10000.0, step=1.0)
